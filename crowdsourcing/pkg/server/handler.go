9060a32d4ca93218aaadc2a6c88e7c408d4543ec
Abolfazl Hosseini <Abolfazl.Hosseini@tapsi.cab>
2025-05-01 10:08:01 +0330

    Add new handler

--- crowdsourcing/pkg/server/handler.go
+++ crowdsourcing/pkg/server/handler.go
@@ -37,15 +37,41 @@ func ConfirmInRideReport(ctx echo.Context) error {
 			log.WithError(err).Errorf("Error confirming Police Report : %v", err)
 			return ctx.String(http.StatusInternalServerError, err.Error())
 		}
-	} else if request.Type == inridereports.FEASIBILITY {
-		err := inridereports.ConfirmFeasibilityReport(request)
-		if err != nil {
-			log.WithError(err).Errorf("Error confirming Feasibility Report : %v", err)
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
 	}
-
 	response := inridereports.SerializeConfirmResponse(nil)
 
 	return ctx.JSON(http.StatusAccepted, response)
+}
+
+func GetVisibilityReport(ctx echo.Context) error {
+
+	type visibilityItem [4]any
+	type visibilityReport []visibilityItem
+
+	reportType := inridereports.POLICE_ONLINE
+
+	allOnlineEvents, err := inridereports.RedisReadAllOnlineReports()
+	if err != nil {
+		log.WithError(err).Errorf("error reading all crowdsourcing reports from redis hash")
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
+
+	var reports = visibilityReport{}
+	for _, events := range allOnlineEvents[reportType] {
+		for _, event := range events {
+			yes := float64(event.PositiveConfirmation)
+			no := float64(event.NegativeConfirmation)
+			confidence := float64((yes * 3) / ((yes * 3) + no))
+
+			report := visibilityItem{
+				event.Location[0],
+				event.Location[1],
+				event.IsAggregated,
+				confidence,
+			}
+
+			reports = append(reports, report)
+		}
+	}
+	return ctx.JSON(http.StatusOK, reports)
 }
