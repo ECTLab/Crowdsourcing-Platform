b0ec72c3cc99cc677380a3540bef14a628dbf05a
Abolfazl Hosseini <Abolfazl.Hosseini@tapsi.cab>
2025-05-01 10:08:18 +0330

    Add visibility endpint

--- crowdsourcing/pkg/server/server.go
+++ crowdsourcing/pkg/server/server.go
@@ -15,6 +15,7 @@ func ServeHttp() {
 	e.Use(middleware.SessionCheckMiddleware())
 	e.POST("/crowdsourcing/in-ride-report", CreateInRideReport)
 	e.POST("/crowdsourcing/in-ride-report/:report_id/confirm", ConfirmInRideReport)
+	e.GET("/crowdsourcing/reports/visibility", GetVisibilityReport)
 	log.Info(fmt.Sprintf("serving on port %d", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
 }
